be16b1cae220ce293d635d6df9003fa14a7e3e27
Joseph Hanna <joseph.com.ec@gmail.com>
2021-05-06 03:08:51 -0500

    Implement updateTodo

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,15 +1,59 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import * as AWS from 'aws-sdk'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient
+const todosTable = process.env.TODOS_TABLE
+
+const logger = createLogger('updateTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('Processing event: ' + event)
+
   const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  console.log(todoId, updatedTodo)
+  logger.info("Update todoId: " + todoId, "Update values: " + updatedTodo)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  await updateTodo(updatedTodo, todoId, userId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: ''
+  }
+}
+
+
+async function updateTodo(updatedTodo: UpdateTodoRequest, todoId: string, userId: string) {
+
+  const params = {
+    TableName: todosTable,
+    Key: {
+      "userId": userId,
+      "todoId": todoId
+    },
+    UpdateExpression: "set #n = :name, dueDate = :dueDate, done = :done",
+    ExpressionAttributeNames: {
+      "#n": "name"
+    },
+    ExpressionAttributeValues: {
+      ":name": updatedTodo.name,
+      ":dueDate": updatedTodo.dueDate,
+      ":done": updatedTodo.done
+    }
+  };
+
+  await docClient.update(params, function (err, data) {
+    if (err) logger.error(err);
+    else logger.info("Success " + data)
+  }).promise()
+
 }
