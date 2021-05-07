de44ebce1b99d6a44637e47ca0ff79b30ca6d76e
Joseph Hanna <joseph.com.ec@gmail.com>
2021-05-07 03:14:44 -0500

    Implement generateUploadUrl

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,12 +1,96 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+const todosTable = process.env.TODOS_TABLE
+const bucketName = process.env.TODOS_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+const logger = createLogger('generateUploadURL')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('Processing event: ' + event)
+
   const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+  const validTodoId = await todoExists(todoId, userId)
 
-  console.log(todoId)
+  if (!validTodoId) {
+    logger.warn("Do not exist, todoId: " + todoId)
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  const signedUrl = getUploadUrl(todoId)
+  await addUrlTodo(todoId, userId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: signedUrl
+    })
+  }
+}
+
+
+async function todoExists(todoId: string, userId: string) {
+  const result = await docClient
+    .get({
+      TableName: todosTable,
+      Key: {
+        todoId: todoId,
+        userId: userId
+      }
+    })
+    .promise()
+
+  logger.info('Get todo: ' + todoId)
+  return !!result.Item
+}
+
+function getUploadUrl(todoId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: parseInt(urlExpiration)
+  })
+}
+
+async function addUrlTodo(todoId: string, userId: string) {
+
+  const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`
+
+  const params = {
+    TableName: todosTable,
+    Key: {
+      "userId": userId,
+      "todoId": todoId
+    },
+    UpdateExpression: "set attachmentUrl = :attachmentUrl",
+    ExpressionAttributeValues: {
+      ":attachmentUrl": attachmentUrl
+    }
+  };
+
+  await docClient.update(params, function (err, data) {
+    if (err) logger.error(err);
+    else logger.info("Success " + data)
+  }).promise()
+
 }
